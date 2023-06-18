910a70be20efe631305116710b3814d5ed578ea1
Bhavesh Kharat <bhaveshkharat.bk@gmail.com>
2023-06-19 00:16:03 +0530

    Updated Contact Us Page

--- assets/js/script.js
+++ assets/js/script.js
@@ -123,17 +123,14 @@ const formBtn = document.querySelector("[data-form-btn]");
 // add event to all form input field
 for (let i = 0; i < formInputs.length; i++) {
   formInputs[i].addEventListener("input", function () {
-
     // check form validation
     if (form.checkValidity()) {
       formBtn.removeAttribute("disabled");
     } else {
       formBtn.setAttribute("disabled", "");
     }
-
   });
 }
-
 
 
 // page navigation variables
